29e632c006e143bb06baacdf6b382bbec40a7976
chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-11-06 21:03:34 +0000

    Added initial files.

---
+++ Manager_api.py
@@ -0,0 +1,74 @@
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Obtaining the current gameweek input 
+current_gw = input("What gameweek? ")
+"""
+Posibly add a code here to check current if entered 0 and go to current event number by looking at history api
+"""
+team_id = input("What is your id? ")
+id_url = 'https://fantasy.premierleague.com/api/bootstrap-static/#/'
+my_team_url = 'https://fantasy.premierleague.com/api/entry/' + team_id + '/event/' + \
+    str(current_gw) + '/picks/'
+
+my_team = requests.get(my_team_url)
+r = requests.get(id_url)
+
+json_database = r.json()
+
+
+
+
+    
+ # Obtaining data about your team1
+json_team = my_team.json()
+team_history = json_team['entry_history']
+picks = json_team['picks']
+
+
+# Looking at the overall database of the system
+print("KEYS:")
+player = find_player_code(json_database, "Salah")
+print(player)
+
+# print(json_database['elements'][0]['code'])
+id_df = pd.DataFrame(json_database['elements'])
+print(id_df.keys())
+elements_types_df = pd.DataFrame(json_database['element_types'])
+print(elements_types_df)
+slim_id_df = id_df[['second_name', 'id', 'now_cost']]
+slim_id_df['position'] = slim_id_df.element_type.map(elements_types_df.set_index('id').singular_name)
+#print(slim_id_df)
+picks_df = pd.DataFrame(picks)
+#print('here:', id_df.loc[id_df.id == picks_df.element[1], 'second_name'])
+
+names = []
+price = []
+pos = []
+for i in range(len(picks_df.element)):
+    pri_val = id_df.loc[id_df.id == picks_df.element[i], 'now_cost']
+    val = id_df.loc[id_df.id == picks_df.element[i], 'web_name']
+    pos_id = id_df.loc[id_df.id == picks_df.element[i], 'element_type']
+    print(val)
+    val_name = str(val).split()[1]
+    pri_val = int((str(pri_val).split()[1]))/10
+    pos_id = int((str(pos_id).split()[1]))
+    pos_name = elements_types_df.loc[elements_types_df.id == pos_id, 'plural_name_short']
+    pos_name = str(pos_name).split()[1]
+
+    names.append(val_name)
+    price.append(pri_val)
+    pos.append(pos_name)
+
+picks_df['Name'] = names
+picks_df['Price (£)'] = price
+picks_df['Position'] = pos
+picks_df = picks_df[['Name', 'Position', 'is_captain', 'is_vice_captain', 'Price (£)']]
+print(picks_df)
+
+
+
+
+# functions
---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+import pandas as pd
+import numpy as np
+import fpl_functions as fpl
+import interface
+import warnings
+
+
+#supress warnings
+warnings.filterwarnings("ignore")
+
+# Obtain the user's team ID and the gameweek
+# Obtaining the current gameweek input 
+current_gw = input("What gameweek? ")
+"""
+Posibly add a code here to check current if entered 0 and go to current event number by looking at history api
+"""
+team_id = input("What is your id? ")
+# URL for the overall database and the user's team
+id_url = 'https://fantasy.premierleague.com/api/bootstrap-static/#/'
+my_team_url = 'https://fantasy.premierleague.com/api/entry/' + team_id + '/event/' + \
+    str(current_gw) + '/picks/'
+
+my_team = requests.get(my_team_url)
+r = requests.get(id_url)
+#Create database for all the platers in the system
+json_database = r.json()
+
+ # Obtaining data about your team1
+json_team = my_team.json()
+user_team_history = json_team['entry_history']
+picks = json_team['picks']
+
+# Looking at the overall database of the system
+
+# creating datarame for every player
+main_df = pd.DataFrame(json_database['elements'])
+#print(main_df.keys())
+slim_main_df = main_df[['web_name', 'element_type', 'team', 'now_cost']]
+# Renaming headers
+slim_main_df.rename(columns={'element_type' : 'position', 'web_name' : 'name', 'now_cost' : 'price'}, inplace = True)
+
+# converting position and team id to the actual name.
+for i in range(len(slim_main_df['position'])):
+    slim_main_df['position'][i] = fpl.position(slim_main_df['position'][i])
+
+
+for j in range(len(slim_main_df['team'])):
+    slim_main_df['team'][j] = fpl.find_team(slim_main_df['team'][j], json_database)
+# Converting cost to correct value
+
+slim_main_df['price'] = slim_main_df.loc[:, ('price')]/10.0
+
+interface.main_menu(slim_main_df,main_df)
+
+
