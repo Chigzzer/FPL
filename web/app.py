603c0f94447b99cdcce9183f4e434a90bc8d1891
chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-11-08 20:06:19 +0000

    Starting backend work of getting the points

---
+++ web/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+import requests as rq
+import fpl_functions as fpl
+import pandas as pd
+
+app = Flask(__name__)
+
+api = 'https://fantasy.premierleague.com/api/bootstrap-static/#/'
+
+# points_api = https://fantasy.premierleague.com/api/event/(gwnumber)/live/
+
+data = rq.get(api)
+database = data.json()
+main_df = pd.DataFrame(database['elements'])
+slim_main_df = main_df[['web_name', 'element_type', 'team', 'now_cost', 'total_points']]
+slim_main_df.rename(columns={'element_type' : 'position', 'web_name' : 'name', 'now_cost' : 'price'}, inplace = True)
+for i in range(len(slim_main_df['position'])):
+    slim_main_df['position'][i] = fpl.position(slim_main_df['position'][i])
+
+for j in range(len(slim_main_df['team'])):
+    slim_main_df['team'][j] = fpl.find_team(slim_main_df['team'][j], database)
+
+# Converting cost to correct value
+slim_main_df['price'] = slim_main_df.loc[:, ('price')]/10.0
+
+#getting the current gameweek
+gw = pd.DataFrame(database['events'])
+gws = gw[['id', 'finished']]
+for week, ids in gws.iterrows():
+    if ids['finished'] != True:
+        current_gw = ids['id']
+        break
+
+
+#Trying to get the points each player has each gameweek
+points_api = 'https://fantasy.premierleague.com/api/event/1/live/'
+points_data = rq.get(points_api)
+points_db = points_data.json()
+points_df = pd.DataFrame(points_db['elements'])
+points_df_id = points_df['id'] #gets player id
+
+#Trying to get table of the players points
+points_df_points = points_df['stats']
+print(points_df_points)
+
+#points_df_points = points_df['stats']['total_points']
+#frames = [points_df_id, points_df_points]
+#table = pd.concat(frames)
+#display(table)
+
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", tables = [slim_main_df.to_html(classes='data')], titles=slim_main_df.columns.values)
+
+@app.route("/dbase.html")
+def dbase():
+    return render_template("index.html", tables = [slim_main_df.to_html(classes='data')], titles=slim_main_df.columns.values)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ web/fpl_functions.py
@@ -0,0 +1,62 @@
+
+def find_player_code(database, name):
+    for i in database['elements']:
+        if i['web_name'] == name:
+            code = i['code']
+    return code
+
+
+def find_player_name(database, code):
+    for i in database['elements']:
+        if i['code'] == code:
+            name= i['web_name']
+    return name
+
+def position(number):
+    if number == 1:
+        pos = "GK"
+    elif number == 2:
+        pos = "DEF"
+    elif number == 3:
+        pos = "MID"
+    else:
+        pos = "FWD"
+    return pos
+
+def find_team(code, database):
+    for i in database['teams']:
+        if i['id'] ==  code:
+            name = i['name']
+            return str(name)
+
+
+# Search functions for each row
+def filter_name(df, name):
+    df = df.query('name == @name')
+    return df
+
+    
+def filter_team(df, team):
+    df = df.query('team == @team')
+    return df
+
+def filter_pos(df, pos):
+    df = df.query('position == @pos')
+    return df
+
+def reset_df(odf):
+    df = odf
+    return df
+
+# Add fucntions to add columns/renamr column to table.
+def add_column(name, df, main_df):
+    df[name] = main_df[name] 
+    return df
+
+def remove_column(name, df):
+    df = df.drop(name, axis = 1) 
+    return df
+
+def sort_db(name, db):
+    db = db.sort_values(by=['name'], ascending=False)
+    return db
